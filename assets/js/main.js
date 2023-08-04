f9290592ab070f97eed8396bb55220cbad5cf39f
Nahom <101549562+nahom-d54@users.noreply.github.com>
2023-08-04 14:13:25 +0300

    Update:  main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,15 @@
 const hamburger = document.querySelector('#hamburger');
+const root = document.querySelector('body')
+const handWave = document.querySelector('.wave');
 const mobile = document.querySelector('.mobile'); 
 hamburger.addEventListener('click',()=> {
     mobile.classList.toggle('mobile-show');
 })
+
+// observable api
+function callback(entries,observable) {
+    entries.forEach(entry => setTimeout(()=>entry.target.classList.toggle('vis'),500))
+}
+let observer = new IntersectionObserver(callback);
+
+observer.observe(handWave)
